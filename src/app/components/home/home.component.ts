4353e0557bd05adb34616ec63878b168747822a6
miguel.flores <miiguelsantos05@hotmail.com>
2023-05-30 17:22:21 -0400

    Começando a Home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,8 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  shopItems: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
   constructor(private userService: UserService) {}
 
   logOut(): void {
